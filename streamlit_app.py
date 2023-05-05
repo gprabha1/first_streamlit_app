91feb60c7bc009bf7918961a0b99a72ceb6f5afa
gprabha1 <132647322+gprabha1@users.noreply.github.com>
2023-05-05 13:08:17 +0530

    Update streamlit_app.py
    add emoji &Avacado and header.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit
 streamlit.title('my parents new healthy diner')
 
 streamlit.header(" Breakfast menu")
-streamlit.text("omega 3 & bueberry oatmeal")
-streamlit.text("kale,spinach & rocket smoothie")
-streamlit.text("Hard boilded free -range egg")
+streamlit.text(" 🥣 omega 3 & bueberry oatmeal")
+streamlit.text(" 🥗 kale,spinach & rocket smoothie")
+streamlit.text(" 🐔 Hard boilded free -range egg")
+streamlit.text("🥑🍞Avacado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
